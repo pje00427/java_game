fd595a7b30d7f9bd5832bbae32002992d437bf5d
pje00427 <59409769+pje00427@users.noreply.github.com>
2020-06-21 18:01:01 +0900

    Initial commit

---
+++ Run.java
@@ -0,0 +1,12 @@
+package com.kh.hard;
+
+public class Run {
+
+	public static void main(String[] args) {
+		CompExample c = new CompExample();
+		//c.method1();
+		RockPaperScissors r = new RockPaperScissors();
+		r.method1();
+	}
+
+}
